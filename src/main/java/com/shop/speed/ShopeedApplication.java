96ea02850ca21430fca4fbf14d04519caa6b6568
Anky <Anky@ALIPL0183.accionlabs.in>
2019-05-19 17:28:32 +0530

    Project init commit

---
+++ src/main/java/com/shop/speed/ShopeedApplication.java
@@ -0,0 +1,13 @@
+package com.shop.speed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeedApplication.class, args);
+	}
+
+}
